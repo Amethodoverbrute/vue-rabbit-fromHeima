fcbc725d80994d7f9a76237ae2b7c3483900da4d
Amethodoverbrute <1703878911@qq.com>
2025-09-06 20:34:35 +0800

    吸顶导航交互实现

--- src/main.js
+++ src/main.js
@@ -9,12 +9,12 @@ import router from "./router";
 // 引入初始化样式文件
 import "@/styles/common.scss";
 
-// 测试接口函数
-import { getCategory } from "./apis/testAPI";
+// // 测试接口函数
+// import { getCategory } from "./apis/testAPI";
 
-getCategory().then((res) => {
-  console.log(res);
-});
+// getCategory().then((res) => {
+//   console.log(res);
+// });
 
 const app = createApp(App);
 
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>我是Layout首页</div>
 
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <!-- 二级路由出口 -->
@@ -12,6 +13,7 @@
 import LayoutNav from "./components/LayoutNav.vue";
 import LayoutHeader from "./components/LayoutHeader.vue";
 import LayoutFooter from "./components/LayoutFooter.vue";
+import LayoutFixed from "./components/LayoutFixed.vue";
 </script>
 
 <style></style>
