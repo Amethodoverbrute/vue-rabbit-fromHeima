4b424e9d9473ffa8644ba3a9989dda8c1c74b2de
Amethodoverbrute <1703878911@qq.com>
2025-09-07 23:34:12 +0800

    一级分类-激活状态显示以及分类列表渲染

--- src/views/Layout/components/LayoutFixed.vue
+++ src/views/Layout/components/LayoutFixed.vue
@@ -32,7 +32,9 @@ const { y } = useScroll(window);
       <!-- 导航区域 -->
       <ul class="app-header-nav">
         <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
+          <RouterLink active-class="active" :to="`/category/${item.id}`">{{
+            item.name
+          }}</RouterLink>
         </li>
         <!-- <li class="home">
           <RouterLink to="/">首页</RouterLink>
