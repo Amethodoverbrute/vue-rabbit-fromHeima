e5b586599e86d4575e4c4b37edde8039731b580d
Amethodoverbrute <1703878911@qq.com>
2025-10-09 23:21:27 +0800

    本地购物车—列表购物车—全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -39,11 +39,20 @@ export const useCartStore = defineStore(
       item.selected = selected;
     };
 
+    // 全选功能
+    const allCheck = (selected) => {
+      // 把cartList中的每一项的selected都设置为当前的全选框状态
+      cartList.value.forEach((item) => (item.selected = selected));
+    };
+
     // 统计，使用计算属性
     // 1、总的数量，即所有项的count之和
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0));
     // 2、总价，即所有项的count*price之和
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0));
+
+    // 是否全选
+    const isAll = computed(() => cartList.value.every((item) => item.selected));
 
     return {
       cartList,
@@ -51,7 +60,9 @@ export const useCartStore = defineStore(
       delCart,
       allCount,
       allPrice,
+      isAll,
       singleCheck,
+      allCheck,
     };
   },
   {
