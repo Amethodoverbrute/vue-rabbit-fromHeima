2378943422a137448b0737c20bb1e42dc02db73b
Amethodoverbrute <1703878911@qq.com>
2025-10-12 23:17:20 +0800

    结算页—提交订单业务实现

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,7 +1,12 @@
 <script setup>
-import { getCheckInfoAPI } from "@/apis/checkout";
+import { getCheckInfoAPI, createOrderAPI } from "@/apis/checkout";
 import { imageEmits } from "element-plus";
 import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+import { useCartStore } from "@/stores/cartStore";
+
+const cartStore = useCartStore();
+const router = useRouter();
 
 const checkInfo = ref({}); // 订单对象
 const curAddress = ref({}); // 地址对象
@@ -25,11 +30,37 @@ const activeAddress = ref({});
 const switchAddress = (item) => {
   activeAddress.value = item;
 };
-
+// 地址弹框 确认
 const confirm = () => {
   curAddress.value = activeAddress.value;
   showDialog.value = false; // 关闭弹窗
   activeAddress.value = {};
+};
+
+// 创建订单
+const createOrder = async () => {
+  const res = await createOrderAPI({
+    deliveryTimeType: 1,
+    payType: 1,
+    payChannel: 1,
+    buyerMessage: "",
+    goods: checkInfo.value.goods.map((item) => {
+      return {
+        skuId: item.skuId,
+        count: item.count,
+      };
+    }),
+    addressId: curAddress.value.id,
+  });
+  const orderId = res.result.id;
+  router.push({
+    path: "/pay",
+    query: {
+      id: orderId,
+    },
+  });
+  //   更新购物车
+  cartStore.updateNewList();
 };
 </script>
 
@@ -129,7 +160,7 @@ const confirm = () => {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large">提交订单</el-button>
+          <el-button @click="createOrder" type="primary" size="large">提交订单</el-button>
         </div>
       </div>
     </div>
