c3a9e6f030ffadf289af87e0f55acb84c7ba7172
Amethodoverbrute <1703878911@qq.com>
2025-10-14 22:24:06 +0800

    支付结果的展示

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Detail from "@/views/Detail/index.vue";
 import CartList from "@/views/CartList/index.vue";
 import Checkout from "@/views/Checkout/index.vue";
 import Pay from "@/views/Pay/index.vue";
+import PayBack from "@/views/Pay/PayBack.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -53,6 +54,10 @@ const router = createRouter({
     {
       path: "/login",
       component: Login,
+    },
+    {
+      path: "/paycallback",
+      component: PayBack,
     },
   ],
   // 路由滚动行为定制/配置
